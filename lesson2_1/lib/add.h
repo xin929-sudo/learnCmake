09532aee952ffc6ec199f5afcd26cd2d0b770f07
xin <ycx_59@163.com>
2026-06-13 20:39:58 +0800

    静态库制作与链接

---
+++ lesson2_1/lib/add.h
@@ -0,0 +1,6 @@
+#ifndef __ADD_H
+#define __ADD_H
+
+int add(int a,int b);
+
+#endif
---
+++ lesson2_1/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+#include<iostream>
+
+int main() {
+
+    std::cout << "静态库制作与链接" << std::endl;
+
+    int c = add(1,2);
+    return 0;
+}
