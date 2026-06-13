0178f33026d509ef834b0369654511db7ce2aab2
xin <ycx_59@163.com>
2026-06-13 17:44:32 +0800

    init: 初始化 CMake 学习项目基础结构，添加 lesson1_1 和 lesson1_2 示例

---
+++ lesson1_1/main.cpp
@@ -0,0 +1,10 @@
+#include "add.h"
+#include <iostream>
+int main() {
+
+    std::cout << "lesson1_1" << std::endl;
+
+    int c = add(1,2);
+
+    return 0;
+}
---
+++ lesson1_2/main.cpp
@@ -0,0 +1,11 @@
+#include "add.h"
+#include<iostream>
+
+int main() {
+
+    std::cout << "lesson1_2" << std::endl;
+
+    int c = add(1,2);
+
+    return 0;
+}
